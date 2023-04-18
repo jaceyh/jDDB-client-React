5ab993d9b9cfe3568b5836e1c5258461da1c8e87
jaceyh <jacey.hallock@gmail.com>
2023-04-18 12:21:21 -0400

    remove hardcoded movies and add props constraints

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -15,7 +15,17 @@ export const MovieCard = ({ movie, onMovieClick }) => {
 // Here is where we define all the props constraints for the BookCard
 MovieCard.propTypes = {
   movie: PropTypes.shape({
-    title: PropTypes.string
+    Name: PropTypes.string.isRequired,
+    Director: PropTypes.array({
+      Director: director.key,
+      Director: director.key
+    }),
+    ImagePath: PropTypes.string,
+    Tags: PropType.array({
+      Tags: Tags.key,
+      Tags: Tags.key
+    }),
+    Description: PropTypes.string,
   }).isRequired,
   onMovieClick: PropTypes.func.isRequired
 };
